cb0aead479633b39515d9100c0c06757f074d735
brenden burrus <brenden.burrus@gmail.com>
2024-09-12 15:17:37 -0500

    adding in selenium and chromdriver to accurately get html code

--- classes/event_finder_class.py
+++ classes/event_finder_class.py
@@ -1,16 +1,21 @@
 # Contains the class that handles getting scrapted data from the Pokemon Event Finder
 
-import requests
+import time
 import bs4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class PokemonEventFinder:
     def __init__(self, url) -> None:
         self.url = url
         self.events = []
-        self.request = ''
         self.soup = ''
 
     
     def getEvents(self):
-        self.request = requests.get(self.url)
-        self.soup = bs4.BeautifulSoup(self.request.text, 'lxml')
+        driver = webdriver.Chrome()
+        driver.get(self.url)
+        time.sleep(3)
+        page = driver.page_source
+        driver.quit()
+        self.soup = bs4.BeautifulSoup(page, 'lxml')
--- get_events.py
+++ get_events.py
@@ -1,6 +1,7 @@
 # Main Script
-
 from classes.event_finder_class import PokemonEventFinder
+
+
 
 
 event_finder_url = "https://events.pokemon.com/en-us/events?sort=when&near=Manchester,%20MO,%20USA&filters=tcg,tournament,league_challenge,league_cup&maxDistance=50"
@@ -11,5 +12,4 @@ if __name__ == "__main__":
     
     events.getEvents()
     print(events.url)
-    print(events.soup.div)
-    
+    print(events.soup)
